ea013d24798f028311e1a3125ec5111b934a08f6
JacobYoung68 <68jacobyoung@gmail.com>
2020-07-31 00:20:23 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Narrative.Code'),
       ),
       body: Center(
-        child: Text('hello'),
+        child: Text(
+          'hello',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Text('Click'),
       ),
-    ),
-  ));
+    );
+  }
 }
