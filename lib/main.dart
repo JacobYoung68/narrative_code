4326a288efccb80e406bf0abea8b3767e1ad323b
JacobYoung68 <68jacobyoung@gmail.com>
2020-07-31 15:00:43 +0100

    Playing around with lists and stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -6,7 +7,15 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int age = 0;
+  List<int> ageList = [11,14,21,45];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,100 +50,85 @@ class Home extends StatelessWidget {
                     child: Container(
                       padding: EdgeInsets.all(20.0),
                       decoration: BoxDecoration(
-                          color: Colors.grey[400],
+                          color: Colors.grey[800],
                           border: Border.all(
                             width: 10,
                             color: Colors.blue,
                           )),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Expanded(
-                            flex: 1,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    padding: EdgeInsets.all(100),
-                                    color: Colors.amber,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text(
-                                          'Name',
-                                          style: TextStyle(
-                                            fontSize: 64,
-                                          ),
-                                        ),
-                                        Icon(
-                                          Icons.portrait,
-                                          size: 64,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                              fontSize: 16,
+                              letterSpacing: 4,
+                              color: Colors.grey[600],
                             ),
                           ),
-                          Expanded(
-                            flex: 1,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    padding: EdgeInsets.all(100),
-                                    color: Colors.amber,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text(
-                                          'Age',
-                                          style: TextStyle(
-                                            fontSize: 64,
-                                          ),
-                                        ),
-                                        Icon(
-                                          Icons.hourglass_empty,
-                                          size: 64,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
+                          SizedBox(height: 10),
+                          Text(
+                            'Jacob',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.amber[200],
                             ),
                           ),
-                          Expanded(
-                            flex: 1,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    padding: EdgeInsets.all(100),
-                                    color: Colors.amber,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text(
-                                          'Description',
-                                          style: TextStyle(
-                                            fontSize: 64,
-                                          ),
-                                        ),
-                                        Icon(
-                                          Icons.color_lens,
-                                          size: 64,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
+                          SizedBox(height: 30),
+                          Text(
+                            'Age',
+                            style: TextStyle(
+                              fontSize: 16,
+                              letterSpacing: 4,
+                              color: Colors.grey[600],
                             ),
                           ),
+                          SizedBox(height: 10),
+                          Text(
+                            '$age',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.amber[200],
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Text(
+                            'Description',
+                            style: TextStyle(
+                              fontSize: 16,
+                              letterSpacing: 4,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '..........',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.amber[200],
+                            ),
+                          ),
+                          FloatingActionButton(
+                            onPressed: () {
+                              setState(() {
+                                var rng = new Random();
+                                age = ageList[rng.nextInt(ageList.length)];
+                              });
+                            },
+                            child: Icon(
+                              Icons.add,
+                            ),
+                          ),
+                          FloatingActionButton(
+                            onPressed: () {
+                              setState(() {
+                                age = 0;
+                              });
+                            },
+                            child: Icon(
+                              Icons.clear,
+                            ),
+                          )
                         ],
                       ),
                     ),
