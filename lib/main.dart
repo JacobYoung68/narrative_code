ee7e67465b8af751560d4f18dc179b397959f1e1
JacobYoung68 <68jacobyoung@gmail.com>
2020-07-31 12:39:55 +0100

    Scaffolding the layout, draft

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,155 @@ void main() {
   ));
 }
 
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Narrative.Code'),
-      ),
-      body: Center(
-        child: Text(
-          'hello',
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          ),
+        backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          title: Text('Narrative.Code'),
+          backgroundColor: Colors.grey[900],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text('Click'),
-      ),
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          border: Border.all(
+                            width: 10,
+                            color: Colors.green,
+                          )),
+                      child: Text('Hello'),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          border: Border.all(
+                            width: 10,
+                            color: Colors.blue,
+                          )),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: EdgeInsets.all(100),
+                                    color: Colors.amber,
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text(
+                                          'Name',
+                                          style: TextStyle(
+                                            fontSize: 64,
+                                          ),
+                                        ),
+                                        Icon(
+                                          Icons.portrait,
+                                          size: 64,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: EdgeInsets.all(100),
+                                    color: Colors.amber,
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text(
+                                          'Age',
+                                          style: TextStyle(
+                                            fontSize: 64,
+                                          ),
+                                        ),
+                                        Icon(
+                                          Icons.hourglass_empty,
+                                          size: 64,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: EdgeInsets.all(100),
+                                    color: Colors.amber,
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text(
+                                          'Description',
+                                          style: TextStyle(
+                                            fontSize: 64,
+                                          ),
+                                        ),
+                                        Icon(
+                                          Icons.color_lens,
+                                          size: 64,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    border: Border.all(
+                      width: 10,
+                      color: Colors.orange,
+                    )),
+                child: Text('Hello'),
+              ),
+            ),
+          ],
+        ));
   }
 }
