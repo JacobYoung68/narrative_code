6db08e243495ec8f0a0bbfa6c9855372e1d3002a
JacobYoung68 <68jacobyoung@gmail.com>
2020-08-02 02:16:36 +0100

    Separated the form from the main dart file.

---
+++ lib/character_form.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'character.dart';
+
+class CharacterForm extends StatefulWidget {
+  final characters;
+  final Function() notifyParent;
+
+  CharacterForm({this.characters, this.notifyParent});
+
+  @override
+  _CharacterFormState createState() => _CharacterFormState();
+}
+
+class _CharacterFormState extends State<CharacterForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name;
+
+  int _age;
+
+  String _description;
+
+  Widget _buildName() {
+    return TextFormField(
+      decoration: InputDecoration(hintText: 'Name'),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Name is required.';
+        }
+        return null;
+      },
+      onSaved: (value) {
+        _name = value;
+      },
+    );
+  }
+
+  Widget _buildAge() {
+    return TextFormField(
+      decoration: InputDecoration(hintText: 'Age'),
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        WhitelistingTextInputFormatter.digitsOnly
+      ],
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Age is required.';
+        }
+        return null;
+      },
+      onSaved: (value) {
+        _age = int.parse(value);
+      },
+    );
+  }
+
+  Widget _buildDescription() {
+    return TextFormField(
+      decoration: InputDecoration(hintText: 'Description'),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Description is required.';
+        }
+        return null;
+      },
+      onSaved: (value) {
+        _description = value;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
+        width: 500,
+        padding: EdgeInsets.all(30),
+        color: Colors.grey,
+        child: Column(
+          children: [
+            Text('Create New Character'),
+            _buildName(),
+            _buildAge(),
+            _buildDescription(),
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  _formKey.currentState.save();
+                  setState(() {
+                    widget.characters.add(Character(
+                        name: '$_name',
+                        age: int.parse('$_age'),
+                        description: '$_description'));
+                  });
+                }
+                widget.notifyParent();
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'character.dart';
 import 'location.dart';
 import 'character_card.dart';
 import 'location_card.dart';
 import 'left_menu.dart';
+import 'character_form.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -50,58 +50,8 @@ class _HomeState extends State<Home> {
     });
   }
 
-  final _formKey = GlobalKey<FormState>();
-  String _name;
-  int _age;
-  String _description;
-
-  Widget _buildName() {
-    return TextFormField(
-      decoration: InputDecoration(hintText: 'Name'),
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'Name is required.';
-        }
-        return null;
-      },
-      onSaved: (value) {
-        _name = value;
-      },
-    );
-  }
-
-  Widget _buildAge() {
-    return TextFormField(
-      decoration: InputDecoration(hintText: 'Age'),
-      keyboardType: TextInputType.number,
-      inputFormatters: <TextInputFormatter>[
-        WhitelistingTextInputFormatter.digitsOnly
-      ],
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'Age is required.';
-        }
-        return null;
-      },
-      onSaved: (value) {
-        _age = int.parse(value);
-      },
-    );
-  }
-
-  Widget _buildDescription() {
-    return TextFormField(
-      decoration: InputDecoration(hintText: 'Description'),
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'Description is required.';
-        }
-        return null;
-      },
-      onSaved: (value) {
-        _description = value;
-      },
-    );
+  refresh() {
+    setState(() {});
   }
 
   @override
@@ -156,53 +106,24 @@ class _HomeState extends State<Home> {
                                 )),
                             child: Row(
                               children: [
+                                CharacterForm(
+                                  characters: characters,
+                                  notifyParent: refresh,
+                                ),
                                 Container(
-                                    child: Form(
-                                  key: _formKey,
-                                  child: Container(
-                                    width: 500,
-                                    padding: EdgeInsets.all(30),
-                                    color: Colors.grey,
-                                    child: Column(
-                                      children: [
-                                        Text('Create New Character'),
-                                        _buildName(),
-                                        _buildAge(),
-                                        _buildDescription(),
-                                        FlatButton(
-                                          color: Colors.red,
-                                          onPressed: () {
-                                            if (_formKey.currentState
-                                                .validate()) {
-                                              _formKey.currentState.save();
-                                              setState(() {
-                                                characters.add(Character(
-                                                    name: '$_name',
-                                                    age: int.parse('$_age'),
-                                                    description:
-                                                        '$_description'));
-                                              });
-                                              print(characters[2].name);
-                                            }
-                                          },
-                                          child: Text('Submit'),
-                                        ),
-                                      ],
+                                  child: Expanded(
+                                    child: GridView.count(
+                                      crossAxisCount: 2,
+                                      children: characters
+                                          .map((character) => CharacterCard(
+                                              character: character,
+                                              delete: () {
+                                                setState(() {
+                                                  characters.remove(character);
+                                                });
+                                              }))
+                                          .toList(),
                                     ),
-                                  ),
-                                )),
-                                Expanded(
-                                  child: GridView.count(
-                                    crossAxisCount: 2,
-                                    children: characters
-                                        .map((character) => CharacterCard(
-                                            character: character,
-                                            delete: () {
-                                              setState(() {
-                                                characters.remove(character);
-                                              });
-                                            }))
-                                        .toList(),
                                   ),
                                 ),
                               ],
